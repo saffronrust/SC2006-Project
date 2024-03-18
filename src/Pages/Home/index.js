4c32973efd0881453840af2785ca104447961379
saffronrust <saffronrust@gmail.com>
2024-03-18 20:08:44 +0800

    added home page content

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,5 +1,24 @@
+import Title from "antd/es/typography/Title";
+
 function Home() {
-    return <div>Home</div>
+    return (
+        <div className="background">
+            <div className="homeSlogan">
+                <Title
+                italic = {true}
+                >
+                    Your BTO journey starts here
+                    </Title>
+            </div>
+            <div className="homeCaption">
+                <Title
+                level={2}
+                >
+                    In Singapore, buying public housing for the first time can be a daunting task for young adults. As such, we have elected to create a convenient and intuitive website for Singaporeans to get easy access to the housing market.
+                </Title>
+            </div>
+        </div>
+    )
 }
 
 export default Home;
