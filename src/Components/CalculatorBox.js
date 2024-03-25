2913a30bc0a94eab484cc21755e4ee73ecf03893
nigelip <nigelip1@gmail.com>
2024-03-25 15:07:00 +0800

    added house price field

--- src/Components/CalculatorBox.js
+++ src/Components/CalculatorBox.js
@@ -1,72 +1,66 @@
+import { FundTwoTone } from '@ant-design/icons';
 import React from 'react';
 import { useState } from "react";
 
 
 const CalculatorBox = () => {
-    const [income, setIncome] = useState("");
+    const [income, setIncome] = useState(null);
     const [cash, setCash] = useState("");
     const [cpf, setCPF] = useState("");
+    const [housePrice, setHousePrice] = useState("");
     
 
     const onButtonClick = () => {
-        console.log ('income = ',income);
-        console.log ('cash = ',cash);
-        console.log ('cpf = ',cpf);
+        // console.log ('income = ',income);
+        // console.log ('cash = ',cash);
+        // console.log ('cpf = ',cpf);
+        // console.log('house price = ', housePrice);
     }
-
 
     return (
     <div className={"topContainer"}>
       <br />
       <br />
-      <div className={"headingContainer"}>
-        <div>Input your Monthly Income</div>
-      </div>
-      <div className={"inputContainer"}>
-        <input
+      <div className={"headingContainer"}>Input your House Price</div>
+      <input
+          value={housePrice}
+          placeholder="Price"
+          onChange={(ev) => setHousePrice(ev.target.value)}
+          className={"inputBox"}
+        />
+      <br />
+      <div className={"headingContainer"}>Input your Monthly Income</div>
+      <input
           value={income}
           placeholder="Monthly Income"
           onChange={(ev) => setIncome(ev.target.value)}
           className={"inputBox"}
         />
-      </div>
       <br />
-      <div className={"headingContainer"}>
-        <div>Input your Disposable* Cash Savings</div>
-      </div>
-      <div className={"inputContainer"}>
-        <input
+      <div className={"headingContainer"}>Input your Disposable* Cash Savings</div>
+      <input
           value={cash}
           placeholder="Cash Savings"
           onChange={(ev) => setCash(ev.target.value)}
           className={"inputBox"}
         />
-      </div>
       <br />
-      <div className={"headingContainer"}>
-        <div>Input your Disposable* CPF Savings</div>
-      </div>
-      <div className={"inputContainer"}>
-        <input
+      <div className={"headingContainer"}>Input your Disposable* CPF Savings</div>
+      <input
           value={cpf}
           placeholder="CPF Savings"
           onChange={(ev) => setCPF(ev.target.value)}
           className={"inputBox"}
         />
-      </div>
       <br />
-      <div className={"infoContainer"}>
-        <div>*amount you would like to use for the flat purchase</div>
-      </div>
+      <div className={"infoContainer"}>*amount you would like to use for the flat purchase</div>
       <br />
-      <div className={"inputContainer"}>
         <input
           className={"inputButton"}
           type="button"
           onClick={onButtonClick}
           value={"Calculate"}
         />
-      </div>
     </div>
   );
 };
