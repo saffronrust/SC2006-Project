dc398c8c165f9d657dc9c2c3f9bfbfe1a5fe407f
saffronrust <saffronrust@gmail.com>
2024-03-27 04:05:25 +0800

    added backup JSON file just in case, added rudimentary input for compare

--- src/Components/ComparisonLayout.js
+++ src/Components/ComparisonLayout.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { Card, Col, Row } from 'antd';
+import { Card, Col, Form, Row } from 'antd';
 import { Input } from 'antd';
+import AutoCompleteInput from './AutoCompleteInput';
+import { Button } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+import { AutoComplete } from 'antd';
+import { flatOptions } from '../Models/Flat';
 
 const { Search } = Input;
+
 
 const onSearch1 = (value, _e, info) => {
     console.log(value);
@@ -18,32 +25,61 @@ const onSearch2 = (value, _e, info) => {
 
 };
 
-const ComparisonLayout = () => (
+function ComparisonLayout() {
 
-    
+    const [flatname, setFlatName] = useState("");
 
+    const handleSubmit = async (values) => {
+        console.log(values);
+        // insert logic to search
+    };
 
-    <Row gutter={16}>
-        <Col span={12} style={{display: 'flex', justifyContent: 'center'}}>
-        <Card style={{width: 700,}}>
+    return (
+        <Row gutter={16}>
+            <Col span={12} style={{display: 'flex', justifyContent: 'center'}}>
+            <Card style={{width: 700,}}>
+                <div className={'headingContainer'}>
+                    House 1 to compare
+                    <Form
+                    onFinish={handleSubmit}
+                    >
+                        <Form.Item
+                        label="Name"
+                        name="flatname"
+                        value={flatname}
+                        onValuesChange={(e) => setFlatName(e.target.value)}
+                        >
+                            <AutoComplete
+                            style={{
+                                width: 200,
+                            }}
+                            options={flatOptions}
+                            placeholder="Bedok North Springs"
+                            filterOption={(inputValue, option) =>
+                                option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+                            }
+                            />
+                            {/* <AutoCompleteInput flatOptions={flatOptions} /> */}
+                        </Form.Item>
+                        <Button type="primary"
+                        icon={<SearchOutlined />}
+                        htmlType="submit">
+                            Search
+                        </Button>
+                    </Form>
+                </div>
+            </Card>
+            </Col>
+            <Col span={12} style={{display: 'flex', justifyContent: 'center'}}>
+            <Card style={{width: 700,}}>
             <div className={'headingContainer'}>
-                House 1 to compare
-                <Search placeholder="input search text" onSearch={onSearch1} enterButton />
-            </div>
-        </Card>
-        </Col>
-        <Col span={12} style={{display: 'flex', justifyContent: 'center'}}>
-        <Card style={{width: 700,}}>
-        <div className={'headingContainer'}>
-                House 2 to compare
-                <Search placeholder="input search text" onSearch={onSearch2} enterButton />
-            </div>
-        </Card>
-        </Col>
-    </Row>
-    
-);
-export default ComparisonLayout;
+                    House 2 to compare
+                    <Search placeholder="input search text" onSearch={onSearch2} enterButton />
+                </div>
+            </Card>
+            </Col>
+        </Row>
+    );
+}
 
-/* <Card title="Card title" bordered={false}> */
-/* <Card style={{width: 700,}}> */
+export default ComparisonLayout;
---
+++ src/Models/Flat/index.js
@@ -0,0 +1,39 @@
+const flatOptions = [
+    {
+      value: 'Bedok North Springs',
+    },
+    {
+      value: 'Bedok South Bloom',
+    },
+    {
+      value: 'Matilda Riverside',
+    },
+    {
+      value: 'Rail Garden',
+    },
+    {
+      value: 'Tanglin Halt Courtyard',
+    },
+    {
+      value: 'Tanjong Tree Residences',
+    },
+    {
+      value: 'Woodgrove Edge',
+    },
+    {
+      value: 'Punggol Central',
+    },
+    {
+      value: 'Punggol North',
+    },
+    {
+      value: 'Punggol South',
+    },
+    {
+      value: 'Punggol East',
+    }
+  ];
+
+  export {
+    flatOptions,
+  }
