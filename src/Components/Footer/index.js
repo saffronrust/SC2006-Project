6d780b88cb967d1a55d2c8708ba54fb4caf0b28a
saffronrust <saffronrust@gmail.com>
2024-03-31 21:32:32 +0800

    changed footer design

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,5 +1,20 @@
+import { Footer } from "antd/es/layout/layout";
+
 function AppFooter() {
-    return <div className="appFooter">Created by JENTS</div>;
+    return (<div className="appFooter">
+      <Footer
+      style={{
+        textAlign: 'center',
+        backgroundColor: 'white',
+        color: 'black',
+        fontSize: '16px',
+        fontWeight: 'bold',
+      }}
+      >
+        A website by Team JENTS for SC2006 Software Engineering Project
+      </Footer>
+      </div>
+    )
   }
 
 export default AppFooter;
