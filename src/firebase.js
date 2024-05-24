dd7a62aa43e6b77b89d9e43166fdd5b347001f0f
saffronrust <saffronrust@gmail.com>
2024-05-24 15:09:54 +0800

    Update firebase.js to put back the keys

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
  * The specific keys have been removed for security reasons
  */
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyAR1UkYZ8gW6iWg00DCfaifqlDxGPOc2dM",
+    authDomain: "simplystay-47876.firebaseapp.com",
+    projectId: "simplystay-47876",
+    storageBucket: "simplystay-47876.appspot.com",
+    messagingSenderId: "1006326958985",
+    appId: "1:1006326958985:web:c626938818ae5a00b12f58",
+    measurementId: "G-276D8ZFB25"
 };
 
 const app = initializeApp(firebaseConfig);
