1c7e6073654e4cf46794fdfa58d81f2e61204575
Zheng Jie <zhengjie@istaging.com>
2024-07-18 17:50:22 +0800

    added firebase API keys to env file

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
  * The specific keys have been removed for security reasons
  */
 const firebaseConfig = {
-    apiKey: "AIzaSyAR1UkYZ8gW6iWg00DCfaifqlDxGPOc2dM",
-    authDomain: "simplystay-47876.firebaseapp.com",
-    projectId: "simplystay-47876",
-    storageBucket: "simplystay-47876.appspot.com",
-    messagingSenderId: "1006326958985",
-    appId: "1:1006326958985:web:c626938818ae5a00b12f58",
-    measurementId: "G-276D8ZFB25"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
